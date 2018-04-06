0c20358c073a2b1f7c0af2e5bbc90b82cd96b6b4
Keith Gable <keith@art19.com>
2018-04-06 12:51:49 -0700

    Bump Loggly cert checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@ default['loggly']['log_dirs'] = []
 default['loggly']['tls']['enabled'] = true
 default['loggly']['tls']['cert_path'] = '/etc/rsyslog.d/keys/ca.d'
 default['loggly']['tls']['cert_url'] = 'https://logdog.loggly.com/media/logs-01.loggly.com_sha12.crt'
-default['loggly']['tls']['cert_checksum'] = 'b562ae82b54bcb43923290e78949153c0c64910d40b02d2207010bb119147ffc'
+default['loggly']['tls']['cert_checksum'] = '3bcd557ea8e43599d3fb98fd13857c2fbff5637cb307a3d4bae23e3c8b5c34cc'
 default['loggly']['tls']['intermediate_cert_url'] = 'https://certs.starfieldtech.com/repository/sf_bundle.crt'
 default['loggly']['tls']['intermediate_cert_checksum'] = '87d171b3333ca95a98aa02603fdb6508939c63f69e14c8587bd66c4f4df65032'
 
